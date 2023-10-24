790bb974d01ca4c2a7369eca4a20547089b12fa0
Mamta <mamtajhamat27@gmail.com>
2023-10-24 14:07:50 +0300

    Update gamezerocross.js

--- gamezerocross.js
+++ gamezerocross.js
@@ -1,39 +1,58 @@
-var restart = document.querySelector("#b")
-var squares = document.querySelectorAll('td')
-function clearBoard(){
-    for (var i = 0; i<squares.length; i++){
+var restart = document.querySelector("#b");
+var squares = document.querySelectorAll('td');
+var currentPlayer = "X";
+var gameOver = false;
+
+function clearBoard() {
+    for (var i = 0; i < squares.length; i++) {
         squares[i].textContent = '';
     }
+    currentPlayer = "X";
+    gameOver = false;
 }
 
 restart.addEventListener('click', clearBoard);
 
-// var cellOne= document.querySelector('#one');
-// cellOne.addEventListener('click', function(){
-//     if (cellOne.textContent === ''){
-//         cellOne.textContent ='X';
-//     }
-//     else if (cellOne.textContent === 'X'){
-//         cellOne.textContent= 'O';
-//     }
-//     else{
-//         cellOne.textContent= '';
-//     }
-// })
+function checkForWin() {
+    // Define winning combinations (rows, columns, and diagonals)
+    var winCombos = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6]
+    ];
 
+    for (var i = 0; i < winCombos.length; i++) {
+        var [a, b, c] = winCombos[i];
+        if (squares[a].textContent === currentPlayer && squares[b].textContent === currentPlayer && squares[c].textContent === currentPlayer) {
+            gameOver = true;
+            alert(currentPlayer + " wins!");
+            clearBoard();
+        }
+    }
 
-function changeMarker(){
-    if (this.textContent === ''){
-        this.textContent = 'X';
+    if (!gameOver && Array.from(squares).every(square => square.textContent !== '')) {
+        // If there's no winner and all squares are filled, it's a draw
+        gameOver = true;
+        alert("It's a draw! No winner.");
+        clearBoard(); // Clear the board when it's a draw
     }
-    else if (this.textContent === 'X'){
-        this.textContent = 'O';
-    }
-    else{
-        this.textContent = '';
-    }
-    
 }
-for (var i = 0; i<squares.length; i++){
-    squares[i].addEventListener('click', changeMarker)
+
+function changeMarker() {
+    if (this.textContent === '' && !gameOver) {
+        this.textContent = currentPlayer;
+        checkForWin(); // Check for a win after each move
+        if (!gameOver) {
+            currentPlayer = currentPlayer === "X" ? "O" : "X"; // Toggle between X and O
+        }
+    }
+}
+
+for (var i = 0; i < squares.length; i++) {
+    squares[i].addEventListener('click', changeMarker);
 }
